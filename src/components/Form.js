948c338c37b0ab426939d1ab9e5927f59f10fa4c
Akshay P V <imapv2000@gmail.com>
2021-03-07 19:39:50 +0530

    Final commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -25,7 +25,7 @@ const Form = ({inputText, setInputText, todos, setTodos, setStatus}) => {
         <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
+          <option value="uncompleted">Pending</option>
         </select>
       </div>
     </form>
